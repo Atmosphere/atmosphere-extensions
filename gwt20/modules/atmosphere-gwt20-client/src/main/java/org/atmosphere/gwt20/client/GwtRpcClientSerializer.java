5d3818b7b6f4bedb168fc7fccbb00a7d42ac6be5
jfarcand <jfarcand@apache.org>
2013-06-28 13:09:43 -0400

    raw contains something, but deserialize fail, so log to see what's happenning

--- gwt20/modules/atmosphere-gwt20-client/src/main/java/org/atmosphere/gwt20/client/GwtRpcClientSerializer.java
+++ gwt20/modules/atmosphere-gwt20-client/src/main/java/org/atmosphere/gwt20/client/GwtRpcClientSerializer.java
@@ -36,6 +36,7 @@ import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamReader;
 import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamWriter;
 import com.google.gwt.user.client.rpc.impl.Serializer;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -43,77 +44,81 @@ import java.util.logging.Logger;
 /**
  * The base class for serializers. To instantiate this class follow this example:
  * <pre><code>
- *
+ * <p/>
  * {@literal @SerialTypes({ MyType1.class, MyType2.class })}
  * public abstract class MyCometSerializer extends AtmosphereGWTSerializer {}
- * 
+ * <p/>
  * AtmosphereGWTSerializer serializer = GWT.create(MyCometSerializer.class);
  * AtmosphereClient client = new AtmosphereClient(url, serializer, listener);
  * </code></pre>
- *
+ * <p/>
  * Where MyType1 and MyType2 are the types that your expecting to receive from the server.
  * If you have a class hierarchy of messages that you want to send you only need to supply the base class here.
- * 
+ * <p/>
  * For instance:
  * <pre><code>
  * public class Message {}
- * 
+ * <p/>
  * public class MessageA extends Message {}
- * 
+ * <p/>
  * public class MessageB extends Message {}
- * 
+ * <p/>
  * {@literal @SerialTypes( Message.class )}
  * public abstract class MyCometSerializer extends AtmosphereGWTSerializer {}
- * 
+ * <p/>
  * </code></pre>
  */
 public abstract class GwtRpcClientSerializer implements ClientSerializer {
-   
-   private static final Logger logger = Logger.getLogger(GwtRpcClientSerializer.class.getName());
-   
-   // buffer in order to capture split messages
-   private StringBuffer buffer = new StringBuffer(16100);
-   
-    
+
+    private static final Logger logger = Logger.getLogger(GwtRpcClientSerializer.class.getName());
+
+    // buffer in order to capture split messages
+    private StringBuffer buffer = new StringBuffer(16100);
+
+
     @Override
     public Object deserialize(String raw) throws SerializationException {
 
-       buffer.append(raw);
-       
-      // split up in different parts - based on the []
-      // this is necessary because multiple objects can be chunked in one single string
-      int brackets = 0;
-      int start = 0;
-      List<String> messages = new ArrayList<String>();
-      for (int i = 0; i < buffer.length(); i++) {
+        buffer.append(raw);
 
-          // detect brackets
-          if (buffer.charAt(i) == '[') {
-             ++brackets;
-          }
-          else if (buffer.charAt(i) == ']') --brackets;
+        // split up in different parts - based on the []
+        // this is necessary because multiple objects can be chunked in one single string
+        int brackets = 0;
+        int start = 0;
+        List<String> messages = new ArrayList<String>();
+        for (int i = 0; i < buffer.length(); i++) {
 
-          // new message
-          if (brackets == 0) {
-              messages.add(buffer.substring(start, i + 1));
-              start = i + 1;
-          }
-      }
-      buffer.delete(0, start);
+            // detect brackets
+            if (buffer.charAt(i) == '[') {
+                ++brackets;
+            } else if (buffer.charAt(i) == ']') --brackets;
 
-      // create the objects
-      List<Object> objects = new ArrayList<Object>(messages.size());
-      for (String message : messages) {
-          try {
-              Serializer serializer = getRPCSerializer();
-              ClientSerializationStreamReader reader = new ClientSerializationStreamReader(serializer);
-              reader.prepareToRead(message);
-              objects.add(reader.readObject());
-          } catch (RuntimeException e) {
-             throw new SerializationException(e);
-          }
-      }
-      return objects;
+            // new message
+            if (brackets == 0) {
+                messages.add(buffer.substring(start, i + 1));
+                start = i + 1;
+            }
+        }
+        buffer.delete(0, start);
+
+        // create the objects
+        List<Object> objects = new ArrayList<Object>(messages.size());
+        for (String message : messages) {
+            try {
+                Serializer serializer = getRPCSerializer();
+                ClientSerializationStreamReader reader = new ClientSerializationStreamReader(serializer);
+                reader.prepareToRead(message);
+                objects.add(reader.readObject());
+            } catch (RuntimeException e) {
+                throw new SerializationException(e);
+            }
+        }
+
+        if (objects.isEmpty()) {
+            logger.fine("No object decoded for " + raw);
+        }
+
+        return objects;
 
     }
 
@@ -129,7 +134,7 @@ public abstract class GwtRpcClientSerializer implements ClientSerializer {
             throw new SerializationException(e);
         }
     }
-    
+
     protected abstract Serializer getRPCSerializer();
 
 }

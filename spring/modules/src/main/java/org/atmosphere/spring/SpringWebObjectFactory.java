7fd0078b1457d4cb439d94c38f20f6c147725d54
apbanerjee <apbanerjee@paypal.com>
2014-04-24 23:15:08 -0700

    Spring DI Injection for Servlet base Java Apps - A new AtmosphereObjectFactory for Spring to be used in Java Apps

---
+++ spring/modules/src/main/java/org/atmosphere/spring/SpringWebObjectFactory.java
@@ -0,0 +1,43 @@
+package org.atmosphere.spring;
+
+import org.atmosphere.cpr.AtmosphereFramework;
+import org.atmosphere.cpr.AtmosphereObjectFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * An {@link AtmosphereObjectFactory} for Spring to be used in Servlet Base Java Apps.
+ *
+ * @author Aparup Banerjee
+ */
+
+public class SpringWebObjectFactory implements AtmosphereObjectFactory {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SpringObjectFactory.class);
+
+	@Override
+	public <T, U extends T> U newClassInstance(AtmosphereFramework framework,
+			Class<T> classType, Class<U> classToInstantiate)
+			throws InstantiationException, IllegalAccessException {
+		WebApplicationContext parent = WebApplicationContextUtils.getWebApplicationContext(framework.getServletContext());
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+	    context.setParent(parent);
+	    context.register(classToInstantiate);
+	    context.refresh();
+		U t = context.getBean(classToInstantiate);
+		if (t == null) {
+			logger.info("Unable to find {}. Creating the object directly."
+					+ classToInstantiate.getName());
+			return classToInstantiate.newInstance();
+		}
+		return t;
+	}
+
+	public String toString() {
+		return "Spring Web ObjectFactory";
+	}
+
+}

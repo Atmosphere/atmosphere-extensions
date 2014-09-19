1fdeb471f3360b3dc6559b0f7c437354a74a89ed
jfarcand <jfarcand@apache.org>
2014-09-19 09:01:46 -0400

    Reformat

--- spring/modules/src/main/java/org/atmosphere/spring/SpringWebObjectFactory.java
+++ spring/modules/src/main/java/org/atmosphere/spring/SpringWebObjectFactory.java
@@ -15,29 +15,29 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  */
 
 public class SpringWebObjectFactory implements AtmosphereObjectFactory {
-	
-	private static final Logger logger = LoggerFactory.getLogger(SpringObjectFactory.class);
 
-	@Override
-	public <T, U extends T> U newClassInstance(AtmosphereFramework framework,
-			Class<T> classType, Class<U> classToInstantiate)
-			throws InstantiationException, IllegalAccessException {
-		WebApplicationContext parent = WebApplicationContextUtils.getWebApplicationContext(framework.getServletContext());
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-	    context.setParent(parent);
-	    context.register(classToInstantiate);
-	    context.refresh();
-		U t = context.getBean(classToInstantiate);
-		if (t == null) {
-			logger.info("Unable to find {}. Creating the object directly."
-					+ classToInstantiate.getName());
-			return classToInstantiate.newInstance();
-		}
-		return t;
-	}
+    private static final Logger logger = LoggerFactory.getLogger(SpringObjectFactory.class);
 
-	public String toString() {
-		return "Spring Web ObjectFactory";
-	}
+    @Override
+    public <T, U extends T> U newClassInstance(AtmosphereFramework framework,
+                                               Class<T> classType, Class<U> classToInstantiate)
+            throws InstantiationException, IllegalAccessException {
+        WebApplicationContext parent = WebApplicationContextUtils.getWebApplicationContext(framework.getServletContext());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.setParent(parent);
+        context.register(classToInstantiate);
+        context.refresh();
+        U t = context.getBean(classToInstantiate);
+        if (t == null) {
+            logger.info("Unable to find {}. Creating the object directly."
+                    + classToInstantiate.getName());
+            return classToInstantiate.newInstance();
+        }
+        return t;
+    }
+
+    public String toString() {
+        return "Spring Web ObjectFactory";
+    }
 
 }

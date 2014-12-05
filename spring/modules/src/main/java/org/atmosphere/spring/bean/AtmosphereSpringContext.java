4988eb4ad611f29adfea381dd4626fc9552e02e1
jfarcand <jfarcand@apache.org>
2014-12-05 11:19:40 -0500

    Add Spring Bean support

---
+++ spring/modules/src/main/java/org/atmosphere/spring/bean/AtmosphereSpringContext.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.spring.bean;
+
+import org.atmosphere.util.ServletProxyFactory;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+public class AtmosphereSpringContext implements ServletConfig {
+
+    private Map<String, String> config;
+
+    @Override
+    public String getServletName() {
+        return "AtmosphereFramework";
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return (ServletContext) Proxy.newProxyInstance(getClass().getClassLoader(), new Class[]{ServletContext.class},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        return ServletProxyFactory.getDefault().proxy(proxy, method, args);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public String getInitParameter(String s) {
+        return config.get(s);
+    }
+
+    @Override
+    public Enumeration<String> getInitParameterNames() {
+        return Collections.enumeration(config.values());
+    }
+
+    public Map<String, String> getConfig() {
+        return config;
+    }
+
+    public void setConfig(Map<String, String> config) {
+        this.config = config;
+    }
+
+}
+
---
+++ spring/modules/src/main/java/org/atmosphere/spring/bean/AtmosphereSpringServlet.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2014 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.spring.bean;
+
+import org.atmosphere.cpr.AtmosphereFramework;
+import org.atmosphere.cpr.AtmosphereRequest;
+import org.atmosphere.cpr.AtmosphereResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AtmosphereSpringServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 6755906261738522768L;
+
+    @Autowired
+    private AtmosphereFramework framework;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        WebApplicationContextUtils.
+                getRequiredWebApplicationContext(getServletContext()).
+                getAutowireCapableBeanFactory().
+                autowireBean(this);
+    }
+
+    @Override
+    public void doHead(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doOptions(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doTrace(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doDelete(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doPut(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        doPost(req, res);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws IOException, ServletException {
+        framework.doCometSupport(AtmosphereRequest.wrap(req), AtmosphereResponse.wrap(res));
+    }
+
+}

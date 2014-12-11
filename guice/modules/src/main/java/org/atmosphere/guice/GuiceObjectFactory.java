53edaddec71b965fba2c25b95ea17bc5b03f289b
Sebastian Lövdahl <slovdahl@hibox.fi>
2014-12-11 11:45:31 +0200

    Refactor GuiceObjectFactory and avoid creating unnecessary injector instances

--- guice/modules/src/main/java/org/atmosphere/guice/GuiceObjectFactory.java
+++ guice/modules/src/main/java/org/atmosphere/guice/GuiceObjectFactory.java
@@ -43,95 +43,94 @@ public class GuiceObjectFactory implements AtmosphereObjectFactory {
     protected AtmosphereConfig config;
 
     @Override
-    public <T, U extends T> U newClassInstance(Class<T> classType, Class<U> classToInstantiate) throws InstantiationException, IllegalAccessException {
-        initInjector(config.framework());
-        U t;
-        if (injector == null) {
-            logger.warn("No Guice Injector found in current ServletContext. Are you using {}?", AtmosphereGuiceServlet.class.getName());
-            logger.trace("Unable to find {}. Creating the object directly.", classToInstantiate.getName());
-            t = classToInstantiate.newInstance();
-        } else {
-            t = injector.getInstance(classToInstantiate);
-        }
-
-        return t;
-    }
-
-    public String toString() {
-        return "Guice ObjectFactory";
-    }
-
-    protected void initInjector(AtmosphereFramework framework) {
-        if (injector == null) {
-            com.google.inject.Injector servletInjector = (com.google.inject.Injector)
-                    framework.getServletContext().getAttribute(com.google.inject.Injector.class.getName());
-
-            if (servletInjector != null) {
-                injector = servletInjector;
-            } else {
-                logger.trace("Creating the Guice injector manually with an empty AbstractModule");
-                injector = Guice.createInjector(new AbstractModule() {
-                    @Override
-                    protected void configure() {
-                    }
-                });
-            }
-
-            injector = injector.createChildInjector(new AbstractModule() {
-                @Override
-                protected void configure() {
-                    bind(BroadcasterFactory.class).toProvider(new Provider<BroadcasterFactory>() {
-                        @Override
-                        public BroadcasterFactory get() {
-                            return config.getBroadcasterFactory();
-                        }
-                    });
-                    bind(AtmosphereFramework.class).toProvider(new Provider<AtmosphereFramework>() {
-                        @Override
-                        public AtmosphereFramework get() {
-                            return config.framework();
-                        }
-                    });
-                    bind(AtmosphereResourceFactory.class).toProvider(new Provider<AtmosphereResourceFactory>() {
-                        @Override
-                        public AtmosphereResourceFactory get() {
-                            return config.resourcesFactory();
-                        }
-                    });
-                    bind(MetaBroadcaster.class).toProvider(new Provider<MetaBroadcaster>() {
-                        @Override
-                        public MetaBroadcaster get() {
-                            return config.metaBroadcaster();
-                        }
-                    });
-                    bind(AtmosphereResourceSessionFactory.class).toProvider(new Provider<AtmosphereResourceSessionFactory>() {
-                        @Override
-                        public AtmosphereResourceSessionFactory get() {
-                            return config.sessionFactory();
-                        }
-                    });
-                    bind(AtmosphereConfig.class).toProvider(new Provider<AtmosphereConfig>() {
-                        @Override
-                        public AtmosphereConfig get() {
-                            return config;
-                        }
-                    });
-
-                }
-            });
-        }
-    }
-
-    @Override
     public void configure(AtmosphereConfig config) {
+        if (config == null) {
+            throw new NullPointerException("AtmosphereConfig can't be null");
+        }
         this.config = config;
 
         try {
-            AtmosphereProducers p = newClassInstance(AtmosphereProducers.class,AtmosphereProducers.class);
+            AtmosphereProducers p = newClassInstance(AtmosphereProducers.class, AtmosphereProducers.class);
             p.configure(config);
         } catch (Exception e) {
             logger.error("", e);
         }
     }
 
+    @Override
+    public <T, U extends T> U newClassInstance(Class<T> classType, Class<U> classToInstantiate) throws InstantiationException, IllegalAccessException {
+        initInjector(config.framework());
+        U t;
+        if (injector != null) {
+            t = injector.getInstance(classToInstantiate);
+        } else {
+            logger.warn("No Guice Injector found in current ServletContext. Are you using {}?", AtmosphereGuiceServlet.class.getName());
+            logger.trace("Unable to find {}. Creating the object directly.", classToInstantiate.getName());
+            t = classToInstantiate.newInstance();
+        }
+
+        return t;
+    }
+
+    @Override
+    public String toString() {
+        return "Guice ObjectFactory";
+    }
+
+    protected void initInjector(AtmosphereFramework framework) {
+        if (injector == null) {
+            // start by trying to get an Injector instance from the servlet context
+            Injector existingInjector = (Injector) framework.getServletContext().getAttribute(Injector.class.getName());
+
+            if (existingInjector != null) {
+                logger.trace("Adding AtmosphereModule to existing Guice injector");
+                injector = existingInjector.createChildInjector(new AtmosphereModule());
+            } else {
+                logger.trace("Creating the Guice injector manually with AtmosphereModule");
+                injector = Guice.createInjector(new AtmosphereModule());
+            }
+        }
+    }
+
+    private class AtmosphereModule extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(BroadcasterFactory.class).toProvider(new Provider<BroadcasterFactory>() {
+                @Override
+                public BroadcasterFactory get() {
+                    return config.getBroadcasterFactory();
+                }
+            });
+            bind(AtmosphereFramework.class).toProvider(new Provider<AtmosphereFramework>() {
+                @Override
+                public AtmosphereFramework get() {
+                    return config.framework();
+                }
+            });
+            bind(AtmosphereResourceFactory.class).toProvider(new Provider<AtmosphereResourceFactory>() {
+                @Override
+                public AtmosphereResourceFactory get() {
+                    return config.resourcesFactory();
+                }
+            });
+            bind(MetaBroadcaster.class).toProvider(new Provider<MetaBroadcaster>() {
+                @Override
+                public MetaBroadcaster get() {
+                    return config.metaBroadcaster();
+                }
+            });
+            bind(AtmosphereResourceSessionFactory.class).toProvider(new Provider<AtmosphereResourceSessionFactory>() {
+                @Override
+                public AtmosphereResourceSessionFactory get() {
+                    return config.sessionFactory();
+                }
+            });
+            bind(AtmosphereConfig.class).toProvider(new Provider<AtmosphereConfig>() {
+                @Override
+                public AtmosphereConfig get() {
+                    return config;
+                }
+            });
+        }
+    }
 }

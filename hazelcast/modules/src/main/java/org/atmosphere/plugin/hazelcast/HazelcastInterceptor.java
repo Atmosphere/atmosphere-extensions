c7cdb6ad044ea0f8b6278c2bf8a6f59cdd58dc4e
Darrel Kleynhans <darrelkley@gmail.com>
2015-02-24 08:12:57 +0200

    Created HazlecastInterceptor that posts the messages onto the same topic on all server. Then after receiving the message from a different server, I follow the same route as I did when receiving a normal message. This will work better then the HazelcastBroadcaster when using MetaBroadcaster methods.

---
+++ hazelcast/modules/src/main/java/org/atmosphere/plugin/hazelcast/HazelcastInterceptor.java
@@ -0,0 +1,51 @@
+package org.atmosphere.plugin.hazelcast;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.Message;
+import com.hazelcast.core.MessageListener;
+import org.atmosphere.cpr.Action;
+import org.atmosphere.cpr.AtmosphereConfig;
+import org.atmosphere.cpr.AtmosphereInterceptorAdapter;
+import org.atmosphere.cpr.AtmosphereResource;
+
+/**
+ * Simple {@link org.atmosphere.cpr.AtmosphereInterceptorAdapter} implementation based on Hazelcast
+ *
+ * @author Darrel Kleynhans
+ */
+public class HazelcastInterceptor extends AtmosphereInterceptorAdapter {
+
+    private static HazelcastInstance HAZELCAST_INSTANCE;
+
+    @Override
+    public void configure(AtmosphereConfig config) {
+        // this can be added to properties file or some place to be configurable.
+        System.setProperty("hazelcast.event.queue.capacity", "10000000000"); 
+        System.setProperty("hazelcast.event.thread.count", "5");
+
+        HAZELCAST_INSTANCE = Hazelcast.newHazelcastInstance();
+        HAZELCAST_INSTANCE.getTopic("distributeMessages").addMessageListener(new MessageListener<Object>() {
+            @Override
+            public void onMessage(Message<Object> inMessage) {
+                if (!inMessage.getPublishingMember().localMember()) {
+                    if (inMessage.getMessageObject() instanceof TopicMessage) {
+                        TopicMessage topicMessage = (TopicMessage) inMessage.getMessageObject();
+                        String message = topicMessage.getMessage();
+                        // Add code as you would normaly here
+
+                    }
+                }
+            }
+        });
+        super.configure(config);
+    }
+
+    @Override
+    public Action inspect(AtmosphereResource resource) {
+        if (!resource.getRequest().body().isEmpty()) {
+            HAZELCAST_INSTANCE.getTopic("distributeMessages").publish(new TopicMessage(resource.getRequest().headersMap(), resource.getBroadcaster().getID(), resource.getRequest().body().asString()));
+        }
+        return Action.CONTINUE;
+    }
+}

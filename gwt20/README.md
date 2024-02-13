416514a64572f66be524d03d5874382d8df35003
jfarcand <jfarcand@apache.org>
2024-02-13 09:14:16 -0500

    Jakarta 3.0.0 support

--- gwt20/README.md
+++
@@ -1,4 +0,0 @@
-
-This is a rework of the GWT modules to become a thin layer on top of Atmosphere to support GWT clients.
-
-See [Wiki](https://github.com/Atmosphere/atmosphere-extensions/wiki/Atmosphere-GWT-2.0)
--- gwt20/modules/atmosphere-gwt20-client/src/main/java/org/atmosphere/gwt20/client/AtmosphereSubscriber.java
+++
@@ -1,9 +0,0 @@
-package org.atmosphere.gwt20.client;
-
-public interface AtmosphereSubscriber {
-    AtmosphereRequest subscribe(RequestConfig requestConfig);
-
-    void unsubscribe();
-    
-    void unsubscribeUrl(String url);
-}
